520c51b9c32ffa693ba92974c2b1865d9b46c8f9
Ryan Benson <benson.ryan@gmail.com>
2018-06-27 21:16:55 -0500

    Creates inventory of items to check and a name reference

--- app.go
+++ app.go
@@ -11,42 +11,56 @@ import (
 )
 
 type Inventory struct {
+  name string
   url string
   dom string
   soldOut string
 }
 
 func StockCheck() {
-  inventory := Inventory{url: "https://www.treesofantiquity.com/index.php?main_page=product_info&products_id=53", dom: "#cartAdd", soldOut: "sold out"}
-  // Request the HTML page.
-  res, err := http.Get(inventory.url)
-  if err != nil {
-    log.Fatal(err)
-  }
-  defer res.Body.Close()
-  if res.StatusCode != 200 {
-    log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
-  }
+  inventory := buildInventory()
 
-  // Load the HTML document
-  doc, err := goquery.NewDocumentFromReader(res.Body)
-  if err != nil {
-    log.Fatal(err)
-  }
-
-  // Find the review items
-  doc.Find("#cartAdd").Each(func(i int, s *goquery.Selection) {
-    // For each item found, get the band and title
-    button_text := strings.ToLower(strings.TrimSpace(s.Find("span").Text()))
-    if(button_text == "sold out") {
-      fmt.Printf("%s : %s", inventory.url, inventory.soldOut)
-    } else {
-      err := beeep.Notify("In Stock Item", inventory.url, "assets/warehouse.png")
-      if err != nil {
-          panic(err)
-      }
+  for _, item := range inventory {
+    // Request the HTML page.
+    res, err := http.Get(item.url)
+    if err != nil {
+      log.Fatal(err)
     }
-  })
+    defer res.Body.Close()
+    if res.StatusCode != 200 {
+      log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+    }
+
+    // Load the HTML document
+    doc, err := goquery.NewDocumentFromReader(res.Body)
+    if err != nil {
+      log.Fatal(err)
+    }
+
+    // Find the review items
+    doc.Find(item.dom).Each(func(i int, s *goquery.Selection) {
+      // For each item found, get the band and title
+      button_text := strings.ToLower(strings.TrimSpace(s.Find("span").Text()))
+      if(button_text == "sold out") {
+        fmt.Printf("%s : %s\n", item.url, item.soldOut)
+      } else {
+        fmt.Printf("%s : %s\n", item.url, "IN STOCK")
+        err := beeep.Notify("In Stock Item", item.url, "assets/warehouse.png")
+        if err != nil {
+            panic(err)
+        }
+      }
+    })
+  }
+}
+
+func buildInventory() []Inventory {
+  fullInventory := []Inventory{
+    Inventory{name: "Golden Russet", url: "https://www.treesofantiquity.com/index.php?main_page=product_info&products_id=53", dom: "#cartAdd", soldOut: "sold out"},
+    Inventory{name: "Black Oxford", url: "https://www.treesofantiquity.com/index.php?main_page=product_info&cPath=1&products_id=639", dom: "#cartAdd", soldOut: "sold out"},
+    Inventory{name: "Tree Starter Package", url: "https://www.treesofantiquity.com/index.php?main_page=product_info&cPath=44&products_id=3", dom: "#cartAdd", soldOut: "sold out"},
+  }
+  return fullInventory
 }
 
 func main() {
