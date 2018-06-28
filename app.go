2be67c405057230378cfc7acfa56cea3b011fc19
Ryan Benson <benson.ryan@gmail.com>
2018-06-27 21:38:17 -0500

    Adds Twilio text message support

--- app.go
+++ app.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+  "os"
   "fmt"
   "log"
   "strings"
+  "net/url"
   "net/http"
+  "encoding/json"
 
   "github.com/PuerkitoBio/goquery"
   "github.com/gen2brain/beeep"
+  _ "github.com/joho/godotenv/autoload"
 )
 
 type Inventory struct {
@@ -49,8 +53,37 @@ func StockCheck() {
         if err != nil {
             panic(err)
         }
+        sendTextMessage()
       }
     })
+  }
+}
+
+func sendTextMessage() {
+  urlStr := "https://api.twilio.com/2010-04-01/Accounts/" + os.Getenv("TWILIO_SID") + "/Messages.json"
+
+  msgData := url.Values{}
+  msgData.Set("To",os.Getenv("TWILIO_PHONE_NUMBER_TO"))
+  msgData.Set("From",os.Getenv("TWILIO_PHONE_NUMBER_FROM"))
+  msgData.Set("Body","SOMETHING IS IN STOCK")
+  msgDataReader := *strings.NewReader(msgData.Encode())
+
+  client := &http.Client{}
+  req, _ := http.NewRequest("POST", urlStr, &msgDataReader)
+  req.SetBasicAuth(os.Getenv("TWILIO_SID"), os.Getenv("TWILIO_AUTH_TOKEN"))
+  req.Header.Add("Accept", "application/json")
+  req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+  resp, _ := client.Do(req)
+  if (resp.StatusCode >= 200 && resp.StatusCode < 300) {
+    var data map[string]interface{}
+    decoder := json.NewDecoder(resp.Body)
+    err := decoder.Decode(&data)
+    if (err == nil) {
+      fmt.Println(data["sid"])
+    }
+  } else {
+    fmt.Println(resp.Status);
   }
 }
 
