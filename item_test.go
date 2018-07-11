267ea44377d4cbcc708be5c13352e7a609d3eb08
Ryan Benson <benson.ryan@gmail.com>
2018-07-11 16:47:02 -0500

    Adds tests for items

--- item_test.go
+++ item_test.go
@@ -12,6 +12,24 @@ func TestItem(t *testing.T) {
   RegisterFailHandler(func(m string, _ ...int){ g.Fail(m) })
 
   g.Describe("#Item", func() {
-    g.It("")
+    g.It("when checking for sold out status, it returns true if the text is matched", func() {
+      i := Item{name: "Apple", url: "http://apples.com", dom: "#soldout", soldOut: "Sold Out"}
+      websiteText := "Sold Out"
+      soldOut := i.isSoldOut(websiteText)
+      Ω(soldOut).Should(BeTrue())
+    })
+
+    g.It("when checking for sold out status, it returns false if the text is not matched", func() {
+      i := Item{name: "Apple", url: "http://apples.com", dom: "#soldout", soldOut: "Sold Out"}
+      websiteText := "Unavailable"
+      soldOut := i.isSoldOut(websiteText)
+      Ω(soldOut).Should(BeFalse())
+    })
+
+    g.It("when processing text, it removes all whitespace, and lowercased", func() {
+      i := Item{name: "Apple", url: "http://apples.com", dom: "#soldout", soldOut: "\n\r Sold Out \n\r"}
+      cleanSoldOut := i.cleanText(i.soldOut)
+      Ω(cleanSoldOut).Should(Equal("sold out"))
+    })
   })
 }
